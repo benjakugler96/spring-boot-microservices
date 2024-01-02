93474982ba7d376484744469f35a23803ee34d2d
benjamin kugler <benjamin.kugler@benjamin.kugler>
2024-01-02 12:25:03 +0100

    feat(order-service): added order service

---
+++ order-service/src/main/java/com/springboot/orderservice/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.springboot.orderservice.service;
+
+import com.springboot.orderservice.dto.OrderLineItemsDto;
+import com.springboot.orderservice.dto.OrderRequest;
+import com.springboot.orderservice.model.Order;
+import com.springboot.orderservice.model.OrderLineItems;
+import com.springboot.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
